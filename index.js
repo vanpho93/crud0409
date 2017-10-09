b5f018264170c4f8642d2555452b6629d823565f
Van Pho Nguyen <vanpho02@gmail.com>
2017-10-09 18:33:23 +0700

    add new product

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const reload = require('reload');
+const parser = require('body-parser').urlencoded({ extended: false });
 const Product = require('./db');
 
 const app = express();
@@ -14,6 +15,18 @@ app.get('/', (req, res) => res.render('home'));
 app.get('/admin', (req, res) => {
     Product.find({})
     .then(products => res.render('admin', { products }))
+    .catch(err => res.send(err.message));
+});
+
+app.get('/add', (req, res) => {
+    res.render('add');
+});
+
+app.post('/add', parser, (req, res) => {
+    const { name, desc, image, video } = req.body;
+    const product = new Product({ name, desc, image, video });
+    product.save()
+    .then(() => res.redirect('/admin'))
     .catch(err => res.send(err.message));
 });
 
